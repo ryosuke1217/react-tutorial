95411713cc7c9f9716f45e5ac00d6298e66cf3b8
r_sasaki <ryosuke121720080711@gmail.com>
2021-01-18 12:19:59 +0900

    add: fetch images from api

--- src/App.js
+++ src/App.js
@@ -1,25 +1,89 @@
-function App() {
+import { useEffect, useState } from "react";
+import { fetchImages } from "./api";
+
+function Header() {
   return (
-    <div>
-      <header>
-        <h1>Cute Dog Images</h1>
-      </header>
-      <main>
-        <section>
-          <figure>
-            <img
-              src="https://images.dog.ceo/breeds/shiba/shiba-8.jpg"
-              alt="cute dog"
-            />
-          </figure>
-        </section>
-      </main>
-      <footer>
+    <header className="hero is-dark is-bold">
+      <div className="hero-body">
+        <div className="container">
+          <h1 className="title">Cute Dog Images</h1>
+        </div>
+      </div>
+    </header>
+  );
+}
+
+function Image(props) {
+  return (
+    <div className="card">
+      <div className="card-image">
+        <figure className="image">
+        <img src={props.src} alt="cute dog!" />
+        </figure>
+      </div>
+    </div>
+  );
+}
+
+function Gallery(props) {
+  const { urls } = props;
+  if (!urls) return <Loading />
+  return (
+    <div className="columns is-vcentered is-multiline">
+      {
+        urls.map((url) => {
+          return (
+            <div key={url} className="column is-3">
+            <Image src={url} />
+          </div>
+          )
+        })
+      }
+    </div>
+  );
+}
+
+function Loading() {
+  return <p>Loading...</p>
+}
+
+function Main() {
+  const [urls, setUrls] = useState(null);
+  useEffect(() => {
+    fetchImages('shiba').then((urls) => {
+      setUrls(urls);
+    });
+  }, []);
+  return (
+    <main>
+      <section className="section">
+        <div className="container">
+          <Gallery urls={urls}/>
+        </div>
+      </section>
+    </main>
+  );
+}
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <div className="content has-text-centered">
         <p>Dog images are retrieved from Dog API</p>
         <p>
           <a href="https://dog.ceo/dog-api/about">Donate to Dog API</a>
         </p>
-      </footer>
+      </div>
+    </footer>
+  );
+}
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Main />
+      <Footer />
     </div>
   );
 }
